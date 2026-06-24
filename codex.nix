23cf5332c334d6833840ebb5280a623e26feb43f
Conor McMaugh <conor@sensorlog.ie>
2026-06-24 14:28:41 +0100

    Update Codex to 0.142.0

--- codex.nix
+++ codex.nix
@@ -10,11 +10,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "codex";
-  version = "0.136.0";
+  version = "0.142.0";
 
   src = fetchurl {
     url = "https://github.com/openai/codex/releases/download/rust-v${finalAttrs.version}/codex-x86_64-unknown-linux-musl.tar.gz";
-    hash = "sha256-2trWydt0UArVQx+heLMCiP292mvfM5djTbbh9ZgTIc8=";
+    hash = "sha256-LjrLOaJ3/xHDFNgyz90kb66+6ia/Aa/46eEGQebeqAE=";
   };
 
   dontUnpack = true;
